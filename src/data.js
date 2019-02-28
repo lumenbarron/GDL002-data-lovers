50140807291f3a4f11ce2bc10498e650eb8db420
lumenbarron <lucemendez@gmail.com>
2019-02-28 12:31:25 -0600

    Tiene todas las fumciones obligatorias, comienzan los test

--- src/data.js
+++ src/data.js
@@ -1,50 +1,52 @@
+
 // Searching for the name of Pokemon
 const searchNamePokemon = (nameInput)=> {
   let namePokemon = "";
     POKEMON.pokemon.find( obj => { 
       if (obj.name.toLowerCase() === nameInput){
-     namePokemon = (obj) //obtenemos un objeto
-    };
+     namePokemon = (obj); 
+    }
   }); 
   return namePokemon;
   };
 
 
 // Searching for the type of Pokemon
-const typePokemon = ( nameType ) => {
+const typePokemonName = ( nameType ) => {
   let typePokemon = POKEMON.pokemon
-    .filter( obj => obj.type[0] === nameType || obj.type[1] === nameType )//obtemos un array mas pequeño con objetos
+    .filter( obj => obj.type[0] === nameType || obj.type[1] === nameType );//obtemos un array mas pequeño con objetos
     return typePokemon;
 };
 
-sortID = "a-z"
-const orderPokemonAlfb = ( typePokemon, sortID) => {
-  console.log("si funciona ");
-  console.log(typePokemon);
-if (sortID == "a-z") {
-  return typePokemon.sort( function (a, b) {
-    if (a.name > b.name) {
-      return 1;
-    }
-  if (a.name < b.name) {
-      return -1;  
-} 
-  return 0
-    )
-
+//sortID = "a-z"
+const orderPokemonAlfb = ( pokemonSorter) => {
+  let sorter = pokemonSorter.sort(function(a,b){
+    if (a.name > b.name ){
+        return 1;
+     } if (a.name < b.name ) {
+        return -1;
+    } 
+  });
+    return sorter;
 };
-document.getElementById("orderPokemon").addEventListener("click",  orderPokemonAlfb );
 
-
-
-
-// Print all the pokemon
-const catalogPokemon = () => {
- 
+//Function to know the average of candy count of all of pokemon (data)
+const averagePokemonCandy = () => {
+  let allPokemon = POKEMON.pokemon
+  .filter(obj => obj.candy_count)
+  .map( obj => obj.candy_count);
+  let reducePokemon = allPokemon.reduce ((accumulator, currentValue) => accumulator + currentValue);
+  let average = reducePokemon/allPokemon.length;
+ console.log(typeof(average));
+ return average;
 };
+
+
 
 
 window.dataLovers = {
-  
-  searchNamePokemon
-}
+   searchNamePokemon: searchNamePokemon,
+   typePokemonName: typePokemonName ,
+   orderPokemonAlfb: orderPokemonAlfb,
+   averagePokemonCandy: averagePokemonCandy
+};
--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
+/*const pagInicio =() => {
+    document.getElementById("inicio").style.display = "block";
+    document.getElementById("resultByType").style.display = "none";
+    document.getElementById("resultByTypeSort").style.display = "none";
+    document.getElementById("searchResult").style.display = "none";
+    };
+
+document.getElementById("back").addEventListener("click", pagInicio);
+
+
+ const pagType =() => {
+    document.getElementById("inicio").style.display = "none";
+    document.getElementById("resultByType").style.display = "block";
+    document.getElementById("resultByTypeSort").style.display = "none";
+    document.getElementById("searchResult").style.display = "none";
+    };
+
+*/
+
 const searchResult = () =>{
     let nameInput = document.getElementById("inputNamePokemon").value.toLowerCase(); 
-    let pokemonImput = searchNamePokemon(nameInput);
+    let pokemonImput = dataLovers.searchNamePokemon(nameInput);
     document.getElementById("searchResult").innerHTML = "Hola! Mi nombre es " + pokemonImput.name + ",soy un pokemon tipo " + pokemonImput.type 
     + " mi principal debilidad son pokemones de tipo " + pokemonImput.weaknesses ;
     document.getElementById("imgImput").src = pokemonImput.img;
@@ -8,12 +27,10 @@ const searchResult = () =>{
 
 document.getElementById("searchName").addEventListener("click", searchResult);
   
-
+let resultTypePokemonPrint;
 const typePokemonPrint = (nameTypeClass) => {
-    console.log("pokemon filtrado");
-    
-    let resultTypePokemonPrint = typePokemon( nameTypeClass );
-    resultTypePokemonPrint.map(element => { element
+   resultTypePokemonPrint = dataLovers.typePokemonName( nameTypeClass );
+    resultTypePokemonPrint.map(element => { element;
         const markup = `
             <div class="type">
             <h2> ${element.name} </h2>
@@ -23,14 +40,38 @@ const typePokemonPrint = (nameTypeClass) => {
             </div>
             `;
             document.getElementById("resultByType").innerHTML += markup;
-            console.log (resultTypePokemonPrint);
-            //return resultTypePokemonPrint;
-        
-    })
-    orderPokemonAlfb ( resultTypePokemonPrint );
+            return resultTypePokemonPrint;  
+    });
 };
 
+const sortPokemonPrint = () => {
+    console.log("imprimir pokemon ordenados");
+    let resultSortPokemonPrint = dataLovers.orderPokemonAlfb(resultTypePokemonPrint );
+    resultSortPokemonPrint.map(element => { element;
+        const markup = `
+            <div class="type">
+            <h2> ${element.name} </h2>
+            <p>
+            <img class="bio" src="${element.img}">
+            </p>
+            </div>
+            `;
+            document.getElementById("resultByType").innerHTML += markup;
+    });
+    dataLovers.orderPokemonAlfb (resultSortPokemonPrint);
+};
+
+document.getElementById("orderPokemon").addEventListener("click", sortPokemonPrint);
+
+const printAverage = () => {
+    let result = dataLovers.averagePokemonCandy();
+    document.getElementById("averageresult").innerHTML = "...Los pokemon necesitan candys para evolucionar?, el promedio de estos es  " + result;
+};
+document.getElementById("average").addEventListener("click",printAverage);
+
 document.getElementById("filterhByTypeGrass").addEventListener("click", function () { typePokemonPrint ("Grass"); });
+//document.getElementById("filterhByTypeGrass").addEventListener("click",pagType );
+//document.getElementById("back").addEventListener("click", pagInicio);
 document.getElementById("filterhByTypeWater").addEventListener("click", function () { typePokemonPrint ("Water"); });
 document.getElementById("filterhByTypePoison").addEventListener("click", function () { typePokemonPrint ("Poison"); });
 document.getElementById("filterhByTypeFire").addEventListener("click", function () { typePokemonPrint ("Fire"); });
